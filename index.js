5d06f570b6105e82bc772478ec3a6fdbf911c351
rendihardi <rendirenhard@gmail.com>
2024-03-05 20:27:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// var myName = "rendi";
+// var userInput = prompt("siapa namamu ?");
+// alert(`nama saya ${userInput}`);
+// alert("nama saya " + userInput);
+
+// var n = math.random();
+// n = n * 6;
+// n = math.floor(n) + 1;
+// console.log(n);
+
+var name = prompt("Siapa namamu?");
+var studentWish = prompt("Mau masuk universitas? (mau/tidak)");
+var chanceGetUniv = Math.random() * 100;
+chanceGetUniv = math.floor(chanceGetUniv);
+
+console.log(chanceGetUniv);
+alert("kemungkinan kamu masuk univ adalah" + chanceGetUniv + " %");
